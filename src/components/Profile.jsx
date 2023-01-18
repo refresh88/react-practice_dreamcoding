18489b13b5cb3b7332b06b555e33ccc552dfd8bb
refresh88 <ouok88@gmail.com>
2023-01-18 22:30:35 +0900

    react 강의 2일차
    component 안에 component 조합하기

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import Avartar from './Avartar';
 
 export default function Profile({ image, name, title, isNew }) {
   return (
     <div className='profile'>
-      <img className='photo' src={image} alt='avatar' />
-      {isNew && <span className='newFriend'>NEW</span>}
+      <Avartar image={image} isNew={isNew} />
       <h1>{name}</h1>
       <p>{title}</p>
     </div>
